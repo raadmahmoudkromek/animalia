d2a0e64bf2730712a4616d49cad63bb06faa8e90
raadmahmoud <raadmahmoud@kromek.com>
2021-08-04 14:34:54 +0100

    Initial commit

---
+++ creatures.py
@@ -0,0 +1,38 @@
+def holy_creation(func):
+    def wrapper(*args, **kwargs):
+        print('I am creating a living thing!')
+        created_thing = func(*args, **kwargs)
+        print('I have... CREATED!!')
+        return created_thing
+
+    return wrapper
+
+
+class Creature:
+    def __init__(self, eyes, diet, legs, arms, holy=False):
+        if holy:
+            self.holy_create(eyes=eyes, diet=diet, legs=legs, arms=arms)
+        else:
+            self.create(eyes=eyes, diet=diet, legs=legs, arms=arms)
+
+    def create(self, eyes, diet, legs, arms):
+        self.apply_properties(eyes=eyes, diet=diet, legs=legs, arms=arms)
+
+    @holy_creation
+    def holy_create(self, eyes, diet, legs, arms):
+        self.apply_properties(eyes=eyes, diet=diet, legs=legs, arms=arms)
+
+    def apply_properties(self, eyes, diet, legs, arms):
+        self.eyes = eyes
+        self.diet = diet
+        self.legs = legs
+        self.arms = arms
+
+    @property
+    def limbs(self):
+        return self.legs + self.arms
+
+
+class Plant:
+    def __init__(self, leaves):
+        self.leaves = leaves
---
+++ testing/unittesting.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestCreatures(unittest.TestCase):
+    def testCreate(self, eyes = 2, arms = 0, legs = 4, diet = 'omni'):
+
+
+
+if __name__ == '__main__':
+    main()
