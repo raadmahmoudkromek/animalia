2640e55db70b7f3c6e29b900e57e934815e19124
raadmahmoud <raadmahmoud@kromek.com>
2022-07-25 12:30:02 +0100

    Added animals, the zoo, unique diet codes enumerator, Started adding fastAPI basic infrastructure.

--- animal_methods/the_zoo.py
+++ animal_methods/the_zoo.py
@@ -1,8 +1,18 @@
+from enum import Enum
+
+
+class EnclosureName(str, Enum):
+    small_mammals = 'small_mammals'
+    reptiles = 'reptiles'
+    primates = 'primates'
+
+
 class enclosure:
-    def __init__(self, enclosure_id, enclosure_size):
+    def __init__(self, enclosure_id, enclosure_size, enclosure_name):
         self.enclosure_id = enclosure_id
         self.enclosure_size = enclosure_size
         self.animals_contained = []
+        self.enclosure_name = enclosure_name
 
     def add_animal(self, animal):
         # add an animal to the enclosure here
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from animal_methods.creatures import holy_creation, DietCodes, Creature, Plant
-from animal_methods import the_zoo
+from animal_methods.the_zoo import EnclosureName
+import logging
 
 app = FastAPI()
+
+logger = logging.getLogger()
 
 
 @app.get("/")
@@ -23,3 +26,20 @@ async def read_user(user_id: str):
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/enclosures/{enclosure_name}")
+async def get_enclosure(enclosure_name: EnclosureName):
+    if enclosure_name == EnclosureName.small_mammals:
+        return {"enclosure_name": enclosure_name, "message": "Welcome to the small mammal enclosure. Cute!"}
+
+    if enclosure_name == EnclosureName.reptiles:
+        return {"enclosure_name": enclosure_name, "message": "Welcome to the reptile enclosure. Hope you like scales!"}
+
+    if enclosure_name == EnclosureName.primates:
+        return {"enclosure_name": enclosure_name, "message": "Welcome to the primate enclosure. The Orang-utans are friendly, but mind the macaques!"}
+
+    else:
+        error_string = "We don't have an enclosure by that name!"
+        logger.error(ValueError())
+        raise ValueError(error_string)
